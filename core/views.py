1147d9ae537f9d74b6f03b9a9d81360d9b049ff4
Amonblessed <ange.diaz@duocuc.cl>
2024-07-04 12:10:03 -0400

    productos

--- core/views.py
+++ core/views.py
@@ -253,11 +253,33 @@ def ventas(request):
 
 @user_passes_test(es_personal_autenticado_y_activo)
 def productos(request, accion, id):
-    form = ProductoForm()
+    
+    if request.method == 'POST':
+        if accion == 'crear':
+            form = ProductoForm(request.POST, request.FILES)
+        if accion == 'actualizar':
+            producto = Producto.objects.get(id=id)
+            form = ProductoForm(request.POST, request.FILES, instance=Producto.objects.get(id=id))
+        if form.is_valid():
+            producto = form.save()
+            ProductoForm(instance=producto)
+            messages.success(request, f'¡El producto {str(producto)} se logró {accion} correctamente!')
+            return redirect(productos, 'actualizar', producto.id)
+        else:
+            messages.error(request, 'No fue posible guardar el producto')
+    if request.method == 'GET':
+        if accion == 'crear':
+            form = ProductoForm()
+        elif accion == 'actualizar':
+            form = ProductoForm(instance=Producto.objects.get(id=id))
+        elif accion == 'eliminar':
+            Producto.objects.get(id=id).delete()
+            messages.success(request, f'¡El producto con ID {id} ha sido eliminado con éxito!')
+            return redirect(productos, 'crear', '0')
     lista= Producto.objects.all()
     return render(request, 'core/productos.html', {
         'form': form,
-        'lista': lista
+        'productos': lista
     })
 
 
